55d30e38f486dd32924bb2c5383716de2a0a6d42
schan <dltjrcks97@gmail.com>
2024-11-20 17:14:48 +0900

    1120-JAVA(p204~),React

--- vs_react/test/src/App.js
+++ vs_react/test/src/App.js
@@ -1,12 +1,64 @@
+import React, { useRef, useState } from 'react';
+import UserList01 from './UserList01';
+import CreateUser from './CreateUser';
 
-import './App.css';
-import Counter from './Counter';
-import InputSample1 from './InputSample1';
-function App(){
-  
-  return( 
-   <InputSample1/>
+function App() {
+  const [inputs, setInputs] = useState({
+    username: '',
+    email: ''
+  });
+  const { username, email } = inputs;
+  const onChange = e => {
+    const { name, value } = e.target;
+    setInputs({
+      ...inputs,
+      [name]: value
+    });
+  };
+  const [users, setUsers] = useState([
+    {
+      id: 1,
+      username: 'velopert',
+      email: 'public.velopert@gmail.com'
+    },
+    {
+      id: 2,
+      username: 'tester',
+      email: 'tester@example.com'
+    },
+    {
+      id: 3,
+      username: 'liz',
+      email: 'liz@example.com'
+    }
+  ]);
+
+  const nextId = useRef(4);
+  const onCreate = () => {
+    const user = {
+      id: nextId.current,
+      username,
+      email
+    };
+    setUsers(users.concat(user));
+
+    setInputs({
+      username: '',
+      email: ''
+    });
+    nextId.current += 1;
+  };
+  return (
+    <>
+      <CreateUser
+        username={username}
+        email={email}
+        onChange={onChange}
+        onCreate={onCreate}
+      />
+      <UserList01 users={users} />
+    </>
   );
-
 }
+
 export default App;
